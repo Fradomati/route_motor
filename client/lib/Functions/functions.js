09fe4d771474f915a4056b3a8c6f478844a817a9
Borja Domínguez <borja.d.atienza@gmail.com>
2020-11-03 08:04:19 +0100

    Working on iFrame Google Maps idea

--- client/lib/Functions/functions.js
+++ client/lib/Functions/functions.js
@@ -16,3 +16,9 @@ export const userAge = (birthday) => {
 
     return dd + (mm * 30) < userDay + (userMonth * 30) ? age - 1 : age
 }
+
+
+export const mapsEmbebed = (url) => {
+
+    return `<iframe src=${url} width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
+}
