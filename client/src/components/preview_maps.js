f938c4975133d3a6b05f9eb8143d6e2efe4053a1
Borja Domínguez <borja.d.atienza@gmail.com>
2020-11-06 08:12:00 +0100

    Mapbox error _typeof...

--- client/src/components/preview_maps.js
+++ client/src/components/preview_maps.js
@@ -3,7 +3,6 @@ import mapboxgl from "mapbox-gl";
 import { getRoute } from "../services/Map_Service";
 
 
-console.log(process.env.TOKEN_MAPBOX)
 mapboxgl.accessToken = process.env.TOKEN_MAPBOX
 
 export const PreviewMaps = (props) => {
@@ -14,7 +13,7 @@ export const PreviewMaps = (props) => {
         [-3.7, 40.39]
     ];
 
-    useEffect(async () => {
+    useEffect(() => {
         const map = new mapboxgl.Map({
             container: test.current,
             style: "mapbox://styles/mapbox/streets-v11",
@@ -22,36 +21,44 @@ export const PreviewMaps = (props) => {
             center: [-3.7, 40.37]
         });
 
-        const response = await getRoute(coor);
-        console.log("Test: ", response);
 
-        const route = response?.data.routes[0].geometry.coordinates;
-        console.log(route);
 
-        map.addLayer({
-            id: "route",
-            type: "line",
-            source: {
-                type: "geojson",
-                data: {
-                    type: "Feature",
-                    properties: {},
-                    geometry: {
-                        type: "LineString",
-                        coordinates: route
-                    }
-                }
-            },
-            layout: {
-                "line-join": "round",
-                "line-cap": "round"
-            },
-            paint: {
-                "line-color": "#3887be",
-                "line-width": 5,
-                "line-opacity": 0.75
-            }
+        let route = null
+        getRoute(coor).then(res => {
+            route = res?.data.routes[0].geometry.coordinates
+            console.log(route);
         });
+        //console.log("Test: ", response);
+
+        // const route = response?.data.routes[0].geometry.coordinates;
+
+        map.on("load", () => {
+
+            map.addLayer({
+                id: "route",
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data: {
+                        type: "Feature",
+                        properties: {},
+                        geometry: {
+                            type: "LineString",
+                            coordinates: route
+                        }
+                    }
+                },
+                layout: {
+                    "line-join": "round",
+                    "line-cap": "round"
+                },
+                paint: {
+                    "line-color": "#3887be",
+                    "line-width": 5,
+                    "line-opacity": 0.75
+                }
+            });
+        })
     }, []);
 
     return <div ref={test} className="mapContainer"></div>;
