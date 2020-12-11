a0848b8069181e095650b8c83d76741c3894f68c
Borja Domínguez <borja.d.atienza@gmail.com>
2020-12-11 08:09:59 +0100

    Working in AddRoute page

--- client/src/components/GoogleMaps/style.js
+++ client/src/components/GoogleMaps/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
 export const IFrame = styled.iframe`
-width: 400px;
-height: 400px
+width: 100%;
+height: 30em;
 `
--- client/src/pages/route/addRoutes/index.js
+++ client/src/pages/route/addRoutes/index.js
@@ -1,7 +1,11 @@
-import React from "react"
+/* ------- ADD ROUTE PAGE ------- */
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+import { urlToCoords, getDirectionsObj } from "../../../../lib/Functions/functions"
+import { GoogleMapsPreview } from "../../../components/GoogleMaps/embed_maps"
 
 // Styles
-import { ParentContainer, CenterContainer } from "../../global_styles"
+import { ParentContainer, ChildContainer, CenterContainer } from "../../global_styles"
 import {
     TitleContainer,
     TitlePage,
@@ -9,26 +13,57 @@ import {
     TitleRoute,
     URLMapContainer,
     URLInput,
-    ButtonSend,
-    ButtonGM
+    Submit,
+    ButtonGM,
+    MapContainer
 } from "./style"
 
 
-export const AddRoute = () => {
+export const AddRoute = (props) => {
+
+    const [coords, setCoords] = useState()
+    const { register, handleSubmit } = useForm({
+        mode: "onSubmit"
+    })
+
+    const filterURL = (data) => {
+
+        const arrPlaces = urlToCoords(data.place)
+        console.log(arrPlaces)
+        // Remove the last element of arrPlaces because its the Center Coords
+        arrPlaces.pop()
+        // Get the object necessary to Google Maps Embed
+        const objDirections = getDirectionsObj(arrPlaces)
+        console.log(objDirections)
+        setCoords(objDirections)
+
+    }
 
     return (
         <ParentContainer>
-            <CenterContainer>
-                <TitleContainer>
-                    <TitlePage>Añadir Nueva Ruta</TitlePage>
-                </TitleContainer>
-                <AddURLContainer>
-                    <TitleRoute>Título</TitleRoute>
-                    <URLMapContainer>
-                        <URLInput /><ButtonSend>Enviar</ButtonSend><ButtonGM>Ir a Google Maps</ButtonGM>
-                    </URLMapContainer>
-                </AddURLContainer>
-            </CenterContainer>
+            <ChildContainer>
+
+                <CenterContainer>
+                    <TitleContainer>
+                        <TitlePage>Añadir Nueva Ruta</TitlePage>
+                    </TitleContainer>
+                    <AddURLContainer>
+                        <TitleRoute>Título</TitleRoute>
+                        <URLMapContainer onSubmit={handleSubmit(filterURL)}>
+                            <URLInput type="text" name="place" ref={register({
+                                required: false
+                            })} /><Submit type="submit" /><ButtonGM>Ir a Google Maps</ButtonGM>
+                        </URLMapContainer>
+                    </AddURLContainer>
+                </CenterContainer>
+            </ChildContainer>
+            <ChildContainer>
+                <CenterContainer>
+                    <MapContainer>
+                        <GoogleMapsPreview coords={coords} />
+                    </MapContainer>
+                </CenterContainer>
+            </ChildContainer>
         </ParentContainer>
     )
 }
