1322b54572a96e75d39df68cbb943e34f8c9b8ff
Borja Domínguez <borja.d.atienza@gmail.com>
2020-12-24 08:09:40 +0100

    Finished the routes page

--- client/src/services/GoogleMaps_Service.js
+++ client/src/services/GoogleMaps_Service.js
@@ -1,41 +1,72 @@
 import { apiGM_GetInfo } from "./Connections"
-import { getSumKLM, getSumDuration, getLocality } from "../../lib/Functions/functions"
+import { getSumKLM, getSumDuration, getLocality, getKLM, fnMountTime } from "../../lib/Functions/functions"
 
 export const getJSONRoute = async (obj) => {
     const { origin, destination, waypoints } = obj
 
-    // Pass waypoints to necesary format
-    let strWaypoints = ""
+    if (waypoints) {
 
-    waypoints.forEach(e => {
-        strWaypoints == "" ? strWaypoints = e : strWaypoints = strWaypoints + "|" + e
-    })
+        // Pass waypoints to necesary format
+        let strWaypoints = ""
 
-    const response = await apiGM_GetInfo.get(
-        `json?origin=${origin}&destination=${destination}&waypoints=${strWaypoints}&key=${process.env.TOKEN_GEO_GOOGLEMAPS}`, {
-    }
-    )
-    console.log("Response", response)
+        console.log(waypoints, "strWaypoints")
+        waypoints.forEach(e => {
+            strWaypoints == "" ? strWaypoints = e : strWaypoints = strWaypoints + "|" + e
+        })
 
-    const route = response.data.routes[0].legs
-    const arrDistances = route.map(e => {
-        return e.distance.value
-    })
-    const arrTiming = route.map(e => {
-        return e.duration.value
-    })
 
-    const totalDistance = getSumKLM(arrDistances)
-    const totalDuration = getSumDuration(arrTiming)
-    const startPoint = route[0].start_address
-    const locality = getLocality(startPoint)
-    console.log("Localidad", locality)
+        const response = await apiGM_GetInfo.get(
+            `json?origin=${origin}&destination=${destination}&waypoints=${strWaypoints}&key=${process.env.TOKEN_GEO_GOOGLEMAPS}`, {
+        }
+        )
+        console.log("Response with waypoints", response)
 
-    return {
-        infoRoute: route,
-        totalDistance: totalDistance,
-        totalDuration: totalDuration,
-        startPoint: startPoint,
-        locality: locality
+        const route = response.data.routes[0].legs
+        const arrDistances = route.map(e => {
+            return e.distance.value
+        })
+        const arrTiming = route.map(e => {
+            return e.duration.value
+        })
+
+        const totalDistance = getSumKLM(arrDistances)
+        const totalDuration = getSumDuration(arrTiming)
+        const startPoint = route[0].start_address
+        const locality = getLocality(startPoint)
+        console.log("Localidad", locality)
+
+        return {
+            infoRoute: route,
+            totalDistance: totalDistance,
+            totalDuration: totalDuration,
+            startPoint: startPoint,
+            locality: locality
+        }
+    } else {
+
+        const response = await apiGM_GetInfo.get(
+            `json?origin=${origin}&destination=${destination}&key=${process.env.TOKEN_GEO_GOOGLEMAPS}`, {
+        }
+        )
+        console.log("Response no waypoints", response)
+
+        const route = response.data.routes[0].legs
+        const distance = route[0].distance.value
+        const timing = route[0].duration.value
+
+        const totalDistance = getKLM(distance)
+        const totalDuration = fnMountTime(timing)
+        const startPoint = route[0].start_address
+        const locality = getLocality(startPoint)
+        console.log("Localidad", locality)
+
+        return {
+            infoRoute: route,
+            totalDistance: totalDistance,
+            totalDuration: totalDuration,
+            startPoint: startPoint,
+            locality: locality
+        }
+
     }
 }
